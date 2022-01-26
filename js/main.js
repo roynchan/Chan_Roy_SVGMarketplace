ecc45eb9a4973f52ab7d3498d07b9580c88e9013
Chan Chun Yip Roy <roynchan@Chans-MacBook-Pro.local>
2022-01-26 07:12:06 -0500

    adding css and html

--- js/main.js
+++ js/main.js
@@ -4,11 +4,14 @@
     console.log('this is some text');
 
     let theHeader = document.querySelector('#main-header');
+    theIcons = document.querySelectorAll(".icon");
 
     function logElement() {
-        console.log('clicked on an element!');
+        console.log('clicked on an element!', this.id);
     }
 
     theHeader.addEventListener('click', logElement);
+
+    theIcons.forEach(icon => icon.addEventListener("click",logElement));
     
 })();
